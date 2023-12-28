1a82dc1a5d2365e059562f26b3c24f4e6e871193
Marcelo Schiavone <marcelo.schiavone@gmail.com>
2023-12-28 12:05:42 -0300

    se agrega cod odoo

--- sort_products.py
+++ sort_products.py
@@ -50,3 +50,74 @@ def sort_products(all_sections, all_products):
     return d
     
 
+def ordering_products_in_sections(all_sections, all_products, all_template_products):
+        ultima_sequence = 1
+        ultima_sequence_sin_seccion = 1000
+        dic_ordenado_de_lineas = {}
+        sections = {}
+        section_anterior = ""
+        for line_template in all_template_products:
+            dic_ordenado_de_lineas[line_template.id] = ultima_sequence
+            if line_template.display_type == "line_section":
+                section_anterior = line_template.name.lower()
+                sections[line_template.name.lower()] = {"inicio": ultima_sequence, "fin": ultima_sequence}
+            else:
+                if section_anterior and section_anterior in sections:  # and not line_template.product_id.section_id:
+                    sections[section_anterior]["fin"] = ultima_sequence
+            ultima_sequence += 1
+
+        for line_section in all_sections:
+            if line_section.name.lower() not in sections:
+                sections[line_section.name.lower()] = {"inicio": ultima_sequence, "fin": ultima_sequence}
+                dic_ordenado_de_lineas[line_section.id] = ultima_sequence
+                ultima_sequence += 1
+            
+        for line_product in all_products:
+            if line_product.product_id.section_id:
+                if (
+                    line_product.product_id.section_id.name.lower()
+                    not in all_sections.mapped(lambda x: x.name.lower())
+                ):
+                    msg = _(
+                        "The product XXX cannot be added because it does not match any Budget section, please check the configuration of the product"
+                    )
+                    msg = msg.replace("XXX", line_product.product_id.name)
+                    raise exceptions.ValidationError(msg)
+                elif line_product.product_id.section_id.name.lower() in sections:
+                    nuevo_fin_section = sections[line_product.product_id.section_id.name.lower()]["fin"] + 1
+                    prod_sequence = nuevo_fin_section
+                    for linea, seq in dic_ordenado_de_lineas.items():
+                        if seq >= prod_sequence:
+                            dic_ordenado_de_lineas[linea] = seq + 1
+                    dic_ordenado_de_lineas[line_product.id] = prod_sequence
+                    # ultima_sequence += 1
+                    for nombre_sec, datos_sec in sections:
+                        if datos_sec["inicio"] >= prod_sequence:
+                            sections[nombre_sec]["inicio"] += 1
+                            sections[nombre_sec]["fin"] += 1
+                        elif datos_sec["inicio"] < prod_sequence <= datos_sec["fin"]:
+                            sections[nombre_sec]["fin"] += 1
+                    sections[line_product.product_id.section_id.name.lower()]["fin"] = nuevo_fin_section
+            else:
+                dic_ordenado_de_lineas[line_product.id] = ultima_sequence_sin_seccion
+                ultima_sequence_sin_seccion += 1
+        return dic_ordenado_de_lineas
+    
+    
+def _ordering_by_sections(self, order_id):
+        all_sections = self.search(
+            [("product_id", "=", None), ("order_id", "=", order_id)], order="id"
+        )
+        all_products = self.search(
+            [("product_id", "!=", None), ("item_from_template", "=", False), ("order_id", "=", order_id)], order="id"
+        )
+        all_template_products = self.search(
+            [("item_from_template", "=", True), ("order_id", "=", order_id)], order="id"
+        )
+        dic_ordenado_de_lineas = self.ordering_products_in_sections(
+            all_sections, all_products, all_template_products
+        )
+        for key, value in dic_ordenado_de_lineas.items():
+            self.browse(key).write({"sequence": value})
+        return
+
